f3b5bfa3873a917e33526562bf65684caead5778
Joseph Handsome <nottellingu@gmail.com>
2024-03-23 11:22:14 -0400

    scraping images fully working

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-import requests
+from src.images import Images
 
-def get_images():
-    raw = requests.get("https://recsports.ufl.edu/cam/cam4.jpg")
-    return raw.content
-
-def str_to_img(raw_str, new_file_name):
-    with open(new_file_name, 'wb') as fo:
-        fo.write(raw_str)
 
 def main():
-    raw_str = get_images()
-    str_to_img(raw_str, "new_file.jpg")
+    Images.get_images()
 
 if __name__ == "__main__":
     main()
