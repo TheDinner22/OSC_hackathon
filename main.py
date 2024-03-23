237849d1d4e02ecaf9c951856f90f0670fdc258b
Joseph Handsome <nottellingu@gmail.com>
2024-03-23 11:03:20 -0400

    ripping images from uf site
    removed unused import

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import requests
+
+def get_images():
+    raw = requests.get("https://recsports.ufl.edu/cam/cam4.jpg")
+    return raw.content
+
+def str_to_img(raw_str, new_file_name):
+    with open(new_file_name, 'wb') as fo:
+        fo.write(raw_str)
+
 def main():
-    print("hi")
+    raw_str = get_images()
+    str_to_img(raw_str, "new_file.jpg")
 
 if __name__ == "__main__":
     main()
