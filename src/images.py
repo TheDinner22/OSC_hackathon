bd0759941c785fafe125b8adb07be821518fa1b4
Joseph Handsome <nottellingu@gmail.com>
2024-03-23 12:05:21 -0400

    no longer save images as jpg
    for 1 its creepy and for 2 i dont need to since the api only needs b64 encoded string

--- src/images.py
+++ src/images.py
@@ -1,24 +1,22 @@
 # scrape and store images in the images dir
 import requests
+import base64
 
 class Images:
     GENERIC_CAM_URL = "https://recsports.ufl.edu/cam/camXXXFILLERXXX.jpg"
     NUMBER_OF_CAMS = 8
 
     @staticmethod
-    # returns nothing but will save the images to folder for later
     def get_images():
-        raw_strs = []
+        b64_images = []
         for cam_num in range(1, 9): # 1, 2, 3, 4, ..., 8
             cam_url = Images.GENERIC_CAM_URL.replace("XXXFILLERXXX", str(cam_num))
             raw = requests.get(cam_url)
-            raw_strs.append(raw.content)
+            b64_images.append(Images._b64_enc_img(raw.content))
 
-        Images._write_images(raw_strs)
+        return b64_images
 
     @staticmethod
-    def _write_images(raw_strs):
-        for i, raw_str in enumerate(raw_strs):
-            file_name = f"images/cam_{str(i+1)}.jpg"
-            with open(file_name, 'wb') as fo:
-                fo.write(raw_str)
+    def _b64_enc_img(raw_str):
+        return base64.b64encode(raw_str)
+
